32ac14dbb017ff0877ddf62ee94fb13362cc7e4f
lenameister <lena.meister27@gmail.com>
2023-03-28 18:10:16 +0200

    gedicht

--- A01_Gedicht/script.js
+++ A01_Gedicht/script.js
@@ -1,2 +1,33 @@
 "use strict";
+var A01;
+(function (A01) {
+    /*
+    Aufgabe 01: Zufallsgedicht
+    Name: Lena Meister
+    Matrikel: 272238
+    Datum: 28.03.2023
+    */
+    let subjects;
+    let predicates;
+    let objects;
+    subjects = ["Lupin", "Snape", "Hagrid", "Ron", "Harry", "Dumbledore"];
+    predicates = ["hasst", "liebt", "schützt", "studiert", "besiegt", "hat"];
+    objects = ["Dementoren", "Lilli", "Schüler", "Magie", "Voldemort", "Macht"];
+    console.log(subjects, predicates, objects);
+    for (let index = subjects.length; index >= 2; index--) {
+        console.log(getVerse(subjects, predicates, objects));
+    }
+    function getVerse(subjects, predicates, objects) {
+        let verse = "";
+        let verseZahl;
+        verseZahl = Math.floor(Math.random() * subjects.length);
+        let versSubjekt = subjects.splice(verseZahl, 1);
+        verseZahl = Math.floor(Math.random() * predicates.length);
+        let versPredikat = predicates.splice(verseZahl, 1);
+        verseZahl = Math.floor(Math.random() * objects.length);
+        let versObjekt = objects.splice(verseZahl, 1);
+        verse = versSubjekt + " " + versPredikat + " " + versObjekt;
+        return verse;
+    }
+})(A01 || (A01 = {}));
 //# sourceMappingURL=script.js.map
--- A01_Gedicht/script.ts
+++ A01_Gedicht/script.ts
@@ -1,10 +1,44 @@
 namespace A01 {
-let sujects: any[] = {"Lena", };
+    /*
+    Aufgabe 01: Zufallsgedicht
+    Name: Lena Meister
+    Matrikel: 272238
+    Datum: 28.03.2023
+    */
+    let subjects: string[];
+    let predicates: string[];
+    let objects: string[];
 
-function name(params:any) {
-    
-    for (let i: any = 0; i < array.length; i++) {
-        
+    subjects = ["Lupin", "Snape", "Hagrid", "Ron", "Harry", "Dumbledore"];
+    predicates = ["hasst", "liebt", "schützt", "studiert", "besiegt", "hat"];
+    objects = ["Dementoren", "Lilli", "Schüler", "Magie", "Voldemort", "Macht"];
+
+    console.log(subjects, predicates, objects);
+
+    for (let index: number = subjects.length; index >= 2; index--) {
+        console.log(getVerse(subjects, predicates, objects));
     }
-}
+
+    function getVerse(subjects: string[], predicates: string[], objects: string[]): string {
+
+        let verse: string = "";
+
+        let verseZahl: number;
+
+        verseZahl = Math.floor(Math.random() * subjects.length);
+        let versSubjekt: string[] = subjects.splice(verseZahl, 1);
+
+        verseZahl = Math.floor(Math.random() * predicates.length);
+        let versPredikat: string[] = predicates.splice(verseZahl, 1);
+
+        verseZahl = Math.floor(Math.random() * objects.length);
+        let versObjekt: string[] = objects.splice(verseZahl, 1);
+
+        verse = versSubjekt + " " + versPredikat + " " + versObjekt;
+
+        return verse;
+
+
+    }
+
 }
