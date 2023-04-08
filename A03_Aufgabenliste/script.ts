54db63e079ced514c34e84d6a32aa9b92dc3cb35
lenameister <lena.meister27@gmail.com>
2023-04-08 15:44:24 +0200

    Aufgabenliste

--- A03_Aufgabenliste/script.ts
+++ A03_Aufgabenliste/script.ts
@@ -3,6 +3,23 @@ namespace A03 {
     Aufgabe 03: Aufgabenliste
     Name: Lena Meister
     Matrikel: 272238
-    Datum: 06.04.2023
+    Datum: 08.04.2023
     */
 }
+
+window.addEventListener("load", handleload);
+function handleload(_event: Event): void {
+
+}
+
+document.querySelector("#btnCreate")!.addEventListener('click', function create() {
+
+    console.log("Aufgabe hinzufügen!");
+
+})
+
+document.querySelector("#btnDelete")!.addEventListener('click', function () {
+
+    console.log("Eintrag löschen!");
+
+})
