a106c9236b21304494b46882cd6f3463581c1a88
lenameister <lena.meister27@gmail.com>
2023-04-24 20:30:11 +0200

    A04

--- A04_Aufgabenliste_Datenstruktur/script.js
+++ A04_Aufgabenliste_Datenstruktur/script.js
@@ -1,11 +1,41 @@
 "use strict";
 window.addEventListener("load", handleload);
 function handleload(_event) {
+    let input = ['person', 'aufg', 'comment', 'date', 'status'];
+    document.querySelector('#btnCreate').addEventListener('click', create);
+    function create() {
+        let neueTask = document.createElement("p");
+        neueTask.innerHTML = input.values;
+        let task = document.getElementById("task");
+        let trash = document.createElement("div");
+        let wrapper = document.createElement("div");
+        wrapper.textContent = "";
+        task.appendChild(wrapper);
+        wrapper.appendChild(neueTask);
+        wrapper?.appendChild(trash);
+        task?.appendChild(wrapper);
+        neueTask.id = "eingabe";
+        wrapper.id = "neuesDiv";
+        trash.className = "fas fa-trash";
+        trash.addEventListener("click", deleteToDo);
+        function deleteToDo() {
+            wrapper.parentElement.removeChild(wrapper);
+        }
+        document.addEventListener("keydown", function (event) {
+            if (event.key == "Enter") {
+                create();
+            }
+        });
+    }
+    /*function create(): void {
+        console.log("Aufgabe hinzugfügen!");
+    }
+    document.querySelector('#btnCreate')!.addEventListener ('click', create);
+
+    
+    function deleteTask(): void {
+        console.log("Aufgabe hinzugfügen!");
+    }
+    document.querySelector('#btnDelete')!.addEventListener ('click', deleteTask);*/
 }
-document.querySelector("#btnCreate").addEventListener('click', function create() {
-    console.log("Aufgabe hinzufügen!");
-});
-document.querySelector("#btnDelete").addEventListener('click', function () {
-    console.log("Eintrag löschen!");
-});
 //# sourceMappingURL=script.js.map
--- A04_Aufgabenliste_Datenstruktur/script.ts
+++ A04_Aufgabenliste_Datenstruktur/script.ts
@@ -1,16 +1,52 @@
 namespace A04 {
     /*
-    Aufgabe 03: Aufgabenliste
+    Aufgabe 03: Aufgabenliste Datenstruktur
     Name: Lena Meister
     Matrikel: 272238
-    Datum: 08.04.2023
+    Datum: 24.04.2023
+    Quellen: Aufgaben aus EIA1, W3S
     */
 }
 
 window.addEventListener("load", handleload);
 function handleload(_event: Event): void {
 
+    let input: string[] = ['person', 'aufg', 'comment', 'date', 'status'];
+
+    document.querySelector('#btnCreate')!.addEventListener('click', create);
     function create(): void {
+
+        let neueTask: HTMLElement = document.createElement("p");
+        neueTask.innerHTML = input.values;
+
+        let task: HTMLElement = document.getElementById("task");
+        let trash: HTMLDivElement = document.createElement("div");
+        let wrapper: HTMLDivElement = document.createElement("div");
+
+        wrapper.textContent = "";
+        task.appendChild(wrapper);
+        wrapper.appendChild(neueTask);
+        wrapper?.appendChild(trash);
+        task?.appendChild(wrapper);
+
+        neueTask.id = "eingabe";
+        wrapper.id = "neuesDiv";
+        trash.className = "fas fa-trash";
+
+        trash.addEventListener("click", deleteToDo);
+
+        function deleteToDo(): void {
+            wrapper.parentElement.removeChild(wrapper);
+        }
+
+        document.addEventListener("keydown", function (event: KeyboardEvent): void { 
+            if (event.key == "Enter") {
+            create();
+            }
+        });
+    }
+
+    /*function create(): void {
         console.log("Aufgabe hinzugfügen!");
     }
     document.querySelector('#btnCreate')!.addEventListener ('click', create);
@@ -19,7 +55,7 @@ function handleload(_event: Event): void {
     function deleteTask(): void {
         console.log("Aufgabe hinzugfügen!");
     }
-    document.querySelector('#btnDelete')!.addEventListener ('click', deleteTask);
+    document.querySelector('#btnDelete')!.addEventListener ('click', deleteTask);*/
 
 
 }
